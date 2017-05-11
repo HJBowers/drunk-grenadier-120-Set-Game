568985b6f85e2a1dd56e2f21d80aa1d628364e26
HJBowers <HJBowers@gmail.com>
2017-05-11 16:05:27 -0700

    can select card within grid

--- src/Board.js
+++ src/Board.js
@@ -9,54 +9,76 @@ export default class Board extends Component {
   constructor(props) {
     super(props)
     this.state= {
-      deck: new Deck(),
-      grid: [null, null, null, null, null, null, null, null, null, null, null, null]
+      deck: {},
+      grid: [],
+      selectedCardsArray: []
     }
+  }
+
+  componentDidMount() {
+    this.newGame()
   }
 
   newGame(){
     let deck= new Deck()
     let grid= [null, null, null, null, null, null, null, null, null, null, null, null]
-    this.setState({ grid, deck })
-    this.fillGrid()
+    let stuff = this.deal(deck, grid)
+    this.setState({ deck: stuff.deck, grid: stuff.grid })
   }
 
-  componentDidMount() {
-    this.fillGrid()
-  }
-
-  fillGrid() {
-    const shuffledDeck = _.shuffle(this.state.deck.cards)
-    let cardsInPlay = this.state.grid.map( (card, index) => {
+  deal(deck, grid) {
+    const shuffledDeck = _.shuffle(deck.cards)
+    let cardsInPlay = grid.map( (card, index) => {
       if (card === null) {
         return card = shuffledDeck.pop()
       }
     })
     let newCards = cardsInPlay.map( (card, index ) => {
-      console.log("card object:::", card);
       return <Card
         image={card.image}
         key={card.index}
-        id={card.number}
+        number={card.number}
         color={card.color}
         shape={card.shape}
         shading={card.shading}
+        // remove unused properties
         selected={card.selected}
         className={card.className}
+        selectedCard={this.selectedCard}
       />
     })
-    this.setState({ grid: newCards, deck: shuffledDeck })
+    return { deck: shuffledDeck, grid: newCards }
+  }
+
+// States and functions within Board Component are not useable within selectedCard function (below).
+  selectedCard(card) {
+    console.log("card:::", card)
+    let emptyArray = []
+    let selectedCardsArray = emptyArray.push( card )
+    console.log("selectedCardsArray", selectedCardsArray)
   }
 
 
+  // selectedArray() {
+  //   console.log("this.selectedCard", this.selectedCard);
+  //   let selectedCardsArray = []
+  //   if (selectedCardsArray.length < 4) {
+  //     selectedCardsArray.push(this.selectedCard)
+  //   } else {
+  //     console.log("this set is full!")
+  //   }
+  //   // this.setState({selectedCards: selectedCardsArray})
+  //   console.log("selectedCardsArray", selectedCardsArray)
+  // }
 
   render() {
-    const deck = this.state.grid
+    console.log("Can I invoke the function?", this.selectedCard())
+    const game = this.state.grid
     return (
       <div>
         <button onClick={this.newGame.bind(this)}>New Game</button>
-        <div className="grid">
-        {deck}
+        <div className="grid" >
+        {game}
         </div>
       </div>
     );
--- src/Card.js
+++ src/Card.js
@@ -5,15 +5,18 @@ import './Card.css';
 export default class Card extends Component {
   constructor(props) {
     super(props)
+    this.clickHandler = this.clickHandler.bind(this)
   }
 
-
+  clickHandler() {
+    this.props.selectedCard(this.props)
+  }
 
 
   render() {
     return (
-      <div className='card'>
-        <img className='img' src={this.props.image} alt='null'/>
+      <div className='card' onClick={this.clickHandler}>
+        <img className='img' src={this.props.image} alt='null' />
       </div>
     )
   }
