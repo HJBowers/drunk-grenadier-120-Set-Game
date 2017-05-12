47a17e7919db2c9bb55b826dc1e2be377823b16d
HJBowers <HJBowers@gmail.com>
2017-05-12 16:23:25 -0700

    Game Logic, comparison functions, alert messages

--- src/Board.js
+++ src/Board.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import _ from 'lodash'
 import Card from './Card'
 import Deck from './Deck'
+import compareSet from './compare'
 
 import './App.css';
 
@@ -11,8 +12,11 @@ export default class Board extends Component {
     this.state= {
       deck: {},
       grid: [],
-      selectedCardsArray: []
+      cardsInPlay: [],
+      selectedCardsArray: [],
+      score: 0,
     }
+    this.selectedCard = this.selectedCard.bind(this)
   }
 
   componentDidMount() {
@@ -20,63 +24,102 @@ export default class Board extends Component {
   }
 
   newGame(){
+    this.setState({selectedCardsArray: []})
     let deck= new Deck()
+    const shuffledDeck = _.shuffle(deck.cards)
     let grid= [null, null, null, null, null, null, null, null, null, null, null, null]
-    let stuff = this.deal(deck, grid)
-    this.setState({ deck: stuff.deck, grid: stuff.grid })
+    let afterDeal = this.deal(shuffledDeck, grid)
+    this.setState({ deck: afterDeal.deck, grid: afterDeal.grid })
   }
+
+   addCards() {
+     let grid = this.state.grid
+     const add3 = [null, null, null]
+     const largeGrid = grid.concat(add3)
+     this.deal()
+     console.log("largeGrid", largeGrid)
+   }
 
   deal(deck, grid) {
-    const shuffledDeck = _.shuffle(deck.cards)
     let cardsInPlay = grid.map( (card, index) => {
       if (card === null) {
-        return card = shuffledDeck.pop()
+        return card = deck.pop()
       }
     })
+    console.log("cardsInPlay::::::", cardsInPlay)
     let newCards = cardsInPlay.map( (card, index ) => {
-      return <Card
-        image={card.image}
-        key={card.index}
-        number={card.number}
-        color={card.color}
-        shape={card.shape}
-        shading={card.shading}
-        // remove unused properties
-        selected={card.selected}
-        className={card.className}
-        selectedCard={this.selectedCard}
-      />
+      return <Card key={`card-${index}`} {...card} selectedCard={this.selectedCard} />
     })
-    return { deck: shuffledDeck, grid: newCards }
+    return { deck: deck, grid: newCards}
   }
 
-// States and functions within Board Component are not useable within selectedCard function (below).
   selectedCard(card) {
-    console.log("card:::", card)
-    let emptyArray = []
-    let selectedCardsArray = emptyArray.push( card )
-    console.log("selectedCardsArray", selectedCardsArray)
+    let score = this.state.score
+    let selectedCards = this.state.selectedCardsArray
+    let cardsInPlay = this.state.cardsInPlay
+    // for (var i = 0; i < this.state.cardsInPlay.length; i++) {
+    //   cardsInPlay[i] = this.state.cardsInPlay[i]
+    //   // console.log("this.state.grid[i].props", this.state.grid[i].props)
+    //   // console.log("this.state.grid[i]", this.state.grid[i])
+    //   // console.log("cardsInPlay[i]", cardsInPlay[i])
+    // }
+    for (var i = 0; i < selectedCards.length; i++) {
+      if( _.isEqual(selectedCards[i], card) ) {
+        selectedCards[i] = card
+      }
+    }
+    this.setState({selectedCardsArray: selectedCards})
+    if (selectedCards.includes(card)) {
+      _.pull(selectedCards, card)
+      // console.log("selectedCards after pull", selectedCards)
+    } else {
+      selectedCards.push(card)
+    }
+    console.log("selectedCards", selectedCards)
+    if (selectedCards.length === 3) {
+    //if set is match, change selcted cards to null in grid and run deal
+      if ( compareSet(selectedCards) ) {
+        alert("You're doing great!")
+        this.setState({score: score + 1})
+        // use the props of the Card Components (cardsInPlay) within the grid to accurtately compare to selected cards
+        for (var i = 0; i < cardsInPlay.length; i++) {
+          if ( _.isEqual(cardsInPlay[i], selectedCards[i]) ) {
+            // console.log("cardsInPlay[i]", cardsInPlay[i].props)
+            // console.log("selectedCards[i]", selectedCards[i])
+            cardsInPlay[i] = null
+          }
+        }
+        console.log("cardsInPlay", cardsInPlay)
+        // let nulledSelectedFromGrid = cardsInPlay.map( gridCard => {
+        //   console.log("gridCard.props", gridCard.props)
+        //   if(_.isEqual(gridCard.props, card)) {
+        //     gridCard.props = null
+        //   }
+        //   console.log("nulledSelectedFromGrid", nulledSelectedFromGrid)
+        // })
+
+        // this.setState( {grid: cardsInPlay} )
+        this.deal()
+      } else {
+        alert("This is not a set")
+        this.setState({selectedCardsArray: []})
+
+      }
+    }
   }
 
-
-  // selectedArray() {
-  //   console.log("this.selectedCard", this.selectedCard);
-  //   let selectedCardsArray = []
-  //   if (selectedCardsArray.length < 4) {
-  //     selectedCardsArray.push(this.selectedCard)
-  //   } else {
-  //     console.log("this set is full!")
-  //   }
-  //   // this.setState({selectedCards: selectedCardsArray})
-  //   console.log("selectedCardsArray", selectedCardsArray)
-  // }
 
   render() {
-    console.log("Can I invoke the function?", this.selectedCard())
     const game = this.state.grid
     return (
       <div>
         <button onClick={this.newGame.bind(this)}>New Game</button>
+        <button onClick={this.addCards.bind(this)}>Add 3 Cards</button>
+        <div className="score">
+          Score:
+          <br/>
+          {this.state.score}
+        </div>
         <div className="grid" >
         {game}
         </div>
